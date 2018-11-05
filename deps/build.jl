3dc2602c13a02105a907e7b48a47c2cb4dfc4f77
mohamed82008 <mohamed82008@gmail.com>
2018-11-05 17:34:55 +1100

    build fixes

--- deps/build.jl
+++ deps/build.jl
@@ -19,13 +19,11 @@ Pkg.add("WriteVTK", v"0.4.0")
 Pkg.add("Tensors")
 
 Pkg.clone("https://github.com/mohamed82008/TopOptProblems.jl")
-Pkg.clone("https://github.com/mohamed82008/TopOpt.jl")
 Pkg.clone("https://github.com/mohamed82008/Optim.jl")
 Pkg.clone("https://github.com/mohamed82008/MMA.jl")
 Pkg.clone("https://github.com/mohamed82008/JuAFEM.jl")
 
 Pkg.checkout("TopOptProblems.jl", "acsimp")
-Pkg.checkout("TopOpt.jl", "acsimp")
 Pkg.checkout("Optim.jl", "acsimp")
 Pkg.checkout("MMA.jl", "acsimp")
 Pkg.checkout("JuAFEM.jl", "acsimp")
