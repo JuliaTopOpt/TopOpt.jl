46cbe6a90b0502b48f5457637c9f9ada3393ab1a
mohamed82008 <mohamed82008@gmail.com>
2018-11-22 23:18:13 +1100

    resources cuda error script

---
+++ test/resources_error.jl
@@ -0,0 +1,23 @@
+using Revise, TopOpt, TopOptProblems, CuArrays, LinearAlgebra
+
+E = 1.0 # Young’s modulus
+v = 0.3 # Poisson’s ratio
+f = 1.0 # downward force
+
+problem = PointLoadCantilever(Val{:Linear}, (160, 40), (1.0, 1.0), E, v, f)
+V = 0.5 # volume fraction
+xmin = 0.001 # minimum density
+maxtol = 0.01 # maximum tolerance
+mintol = 0.0001 # minimum tolerance
+steps = 40 # maximum number of penalty steps, delta_p0 = 0.1
+reuse = true # adaptive penalty flag
+filtering = true
+solver = FEASolver(Displacement, CG, MatrixFree, problem, xmin = xmin,
+	penalty = TopOpt.PowerPenalty(1.0))	
+cusolver = cu(solver)
+cuobj = ComplianceObj(problem, cusolver, filtering = filtering,
+rmin = 3.0, tracing = true, logarithm = false)
+
+operator = TopOpt.buildoperator(cusolver)
+x = CuArray(ones(length(cusolver.f)))
+mul!(x, operator, cusolver.f)
