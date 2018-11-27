9b89d2ceb8ee31b50786e318c547b7a77405251d
mohamed82008 <mohamed82008@gmail.com>
2018-11-28 00:44:09 +1100

    GPU fixes

--- src/fea_solvers/matrix_free_apply_bcs.jl
+++ src/fea_solvers/matrix_free_apply_bcs.jl
@@ -5,21 +5,26 @@ function matrix_free_apply2Kes!(elementinfo::ElementFEAInfo{dim, T}, raw_element
     raw_KK = raw_elementinfo.Kes
 
     ch = problem.ch
-    dof_cells = problem.metadata.dof_cells
-    dof_cells_offset = problem.metadata.dof_cells_offset
+    dof_cells = elementinfo.metadata.dof_cells
+    dof_cells_offset = elementinfo.metadata.dof_cells_offset
 
-    M = zero(T)
-    for i in 1:length(raw_KK)
-        s = sumdiag(raw_KK[i])
+    M = mapreduce(sumdiag, +, raw_KK, init=zero(T))
+    #M = zero(T)
+    #for i in 1:length(raw_KK)
+    #    s = sumdiag(raw_KK[i])
         #M += s * ifelse(black[i], 1, ifelse(white[i], xmin, density(vars[varind[i]], xmin))
-        M += s
-    end
+    #    M += s
+    #end
     M /= length(dof_cells_offset)
-
     m = size(KK[1], 1)
-    for ind in 1:length(ch.values)
-        d = ch.prescribed_dofs[ind]
-        v = ch.values[ind]
+    update_KK!(KK, m, M, ch.values, ch.prescribed_dofs, dof_cells_offset, dof_cells)
+    return M
+end
+
+function update_KK!(KK::Vector, m, M::T, values, prescribed_dofs, dof_cells_offset, dof_cells) where {T}
+    for ind in 1:length(values)
+        d = prescribed_dofs[ind]
+        v = values[ind]
         r = dof_cells_offset[d] : dof_cells_offset[d+1]-1
         for idx in r
             (i,j) = dof_cells[idx]
@@ -46,7 +51,50 @@ function matrix_free_apply2Kes!(elementinfo::ElementFEAInfo{dim, T}, raw_element
             end
         end
     end
-    return M
+    return
+end
+
+function update_KK!(KK::CuVector, m, M, values, prescribed_dofs, dof_cells_offset, dof_cells)
+    args = (KK, m, M, values, prescribed_dofs, dof_cells_offset, dof_cells)
+    callkernel(dev, bc_kernel1, args)
+    CUDAdrv.synchronize(ctx)
+    return 
+end
+
+function bc_kernel1(KK, m, M::T, values, prescribed_dofs, dof_cells_offset, dof_cells) where {T}
+    ind = @thread_global_index()
+    offset = @total_threads()
+    while ind <= length(values)
+        d = prescribed_dofs[ind]
+        v = values[ind]
+        r = dof_cells_offset[d] : dof_cells_offset[d+1]-1
+        for idx in r
+            (i,j) = dof_cells[idx]
+            if eltype(KK) <: Symmetric
+                KKi = KK[i].data
+            else
+                KKi = KK[i]
+            end
+            for col in 1:m
+                for row in 1:m
+                    if row == j || col == j
+                        if row == col
+                            KKi = @set KKi[j,j] = M
+                        else
+                            KKi = @set KKi[row,col] = zero(T)
+                        end
+                    end
+                end
+            end
+            if eltype(KK) <: Symmetric
+                KK[i] = Symmetric(KKi)
+            else
+                KK[i] = KKi
+            end
+        end
+        ind += offset
+    end
+    return
 end
 
 function matrix_free_apply2f!(f::AbstractVector{T}, rawelementinfo::ElementFEAInfo{dim, T}, M, vars, problem::StiffnessTopOptProblem, penalty, xmin, applyzero::Bool=false) where {dim, T}
@@ -60,10 +108,16 @@ function matrix_free_apply2f!(f::AbstractVector{T}, rawelementinfo::ElementFEAIn
     dof_cells_offset = rawelementinfo.metadata.dof_cells_offset
     cell_dofs = rawelementinfo.metadata.cell_dofs
 
-    m = size(raw_KK[1], 1)
-    for ind in 1:length(ch.values)
-        d = ch.prescribed_dofs[ind]
-        v = ch.values[ind]
+    update_f!(f, ch.values, ch.prescribed_dofs, dof_cells_offset, applyzero, dof_cells, cell_dofs, black, white, raw_KK, xmin, penalty, vars, varind, M)
+
+    return
+end
+
+function update_f!(f::Vector{T}, values, prescribed_dofs, dof_cells_offset, applyzero, dof_cells, cell_dofs, black, white, raw_KK, xmin, penalty, vars, varind, M) where {T}
+    for ind in 1:length(values)
+        d = prescribed_dofs[ind]
+        v = values[ind]
+        m = size(raw_KK[ind], 1)
 
         r = dof_cells_offset[d] : dof_cells_offset[d+1]-1
         if !applyzero && v != 0 
@@ -123,6 +177,85 @@ function matrix_free_apply2f!(f::AbstractVector{T}, rawelementinfo::ElementFEAIn
                 end
             end
         end
+    end
+    return 
+end
+
+function update_f!(f::CuVector{T}, values, prescribed_dofs, dof_cells_offset, applyzero, dof_cells, cell_dofs, black, white, raw_KK, xmin, penalty, vars, varind, M) where {T}
+    args = (f, values, prescribed_dofs, dof_cells_offset, applyzero, dof_cells, cell_dofs, black, white, raw_KK, xmin, penalty, vars, varind, M)
+    callkernel(dev, bc_kernel2, args)
+    CUDAdrv.synchronize(ctx)
+    return 
+end
+
+function bc_kernel2(f::AbstractVector{T}, values, prescribed_dofs, dof_cells_offset, applyzero, dof_cells, cell_dofs, black, white, raw_KK, xmin, penalty, vars, varind, M) where {T}
+
+    ind = @thread_global_index()
+    offset = @total_threads()
+    while ind <= length(values)
+        d = prescribed_dofs[ind]
+        v = values[ind]
+        m = size(raw_KK[ind], 1)
+
+        r = dof_cells_offset[d] : dof_cells_offset[d+1]-1
+        if !applyzero && v != 0 
+            for idx in r
+                (i,j) = dof_cells[idx]
+                if black[i]
+                    for row in 1:m
+                        f[cell_dofs[row,i]] -= v * raw_KK[i][row,j]
+                    end
+                elseif white[i]
+                    px = xmin
+                    for row in 1:m
+                        f[cell_dofs[row,i]] -= px * v * raw_KK[i][row,j]
+                    end
+                else
+                    px = density(penalty(vars[varind[i]]), xmin)
+                    for row in 1:m
+                        f[cell_dofs[row,i]] -= px * v * raw_KK[i][row,j]
+                    end
+                end
+            end
+        end
+        f[d] = zero(T)
+        for idx in r 
+            (i,j) = dof_cells[idx]
+            if black[i]
+                for col in 1:m
+                    for row in 1:m
+                        if row == j || col == j
+                            if row == col
+                                f[cell_dofs[j,i]] += M*v
+                            end
+                        end
+                    end
+                end
+            elseif white[i]
+                px = xmin
+                for col in 1:m
+                    for row in 1:m
+                        if row == j || col == j
+                            if row == col
+                                f[cell_dofs[j,i]] += px*M*v #Take out and do most of the rest once
+                            end
+                        end
+                    end
+                end
+            else
+                px = density(penalty(vars[varind[i]]), xmin)
+                for col in 1:m
+                    for row in 1:m
+                        if row == j || col == j
+                            if row == col
+                                f[cell_dofs[j,i]] += px*M*v
+                            end
+                        end
+                    end
+                end
+            end
+        end
+        ind += offset
     end
     return 
 end
