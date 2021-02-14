aee9797e923590d433d9d20cc3f77a2a1feeb928
Mohamed Tarek <mohamed82008@gmail.com>
2021-02-14 20:52:31 +1100

    make Makie optional

--- src/TopOptProblems/Visualization/Visualization.jl
+++ src/TopOptProblems/Visualization/Visualization.jl
@@ -1,10 +1,13 @@
 module Visualization
 
 using ..TopOptProblems: AbstractTopOptProblem, StiffnessTopOptProblem
-using JuAFEM, VTKDataTypes
+using JuAFEM, VTKDataTypes, Requires
 
 # export ...
 include("mesh_types.jl")
-include("makie.jl")
+
+function __init__()
+    @require Makie="ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" @eval include("makie.jl")
+end
 
 end
--- src/TrussTopOptProblems/TrussVisualization/TrussVisualization.jl
+++ src/TrussTopOptProblems/TrussVisualization/TrussVisualization.jl
@@ -1,7 +1,13 @@
 module TrussVisualization
 
-include("makie.jl")
+using Requires
 
-export visualize
+function __init__()
+    @require Makie="ee78f7c6-11fb-53f2-987a-cfe4a2b5a57a" @eval begin
+        include("makie.jl")
+        export visualize
+    end
+end
+
 
 end
