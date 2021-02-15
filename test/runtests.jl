d8aa358b9eff7afc766236fa717a6d51368e35ff
Mohamed Tarek <mohamed82008@gmail.com>
2021-02-15 12:54:46 +1100

    solver tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -10,14 +10,25 @@ end
 @safetestset "Functions" begin
     include("functions.jl")
 end
+# @safetestset "Solver" begin
+#     include("fea/solvers.jl")
+# end
 @safetestset "Truss Problem Tests" begin
     include("truss_topopt_problems/test_problem.jl")
     include("truss_topopt_problems/test_fea.jl")
 end
 @safetestset "Examples" begin
-    @safetestset "CSIMP" include("examples/csimp.jl")
-    @safetestset "BESO" include("examples/beso.jl")
-    @safetestset "GESO" include("examples/geso.jl")
-    @safetestset "Global Stress" include("examples/stress.jl")
+    @safetestset "CSIMP" begin
+        include("examples/csimp.jl")
+    end
+    @safetestset "BESO" begin
+        include("examples/beso.jl")
+    end
+    @safetestset "GESO" begin
+        include("examples/geso.jl")
+    end
+    @safetestset "Global Stress" begin
+        include("examples/stress.jl")
+    end
     #@safetestset "More examples" include("examples/test_examples.jl")
 end
