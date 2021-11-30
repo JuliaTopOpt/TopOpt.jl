a189cbdefe1c27671953f61c5f2c226bdc8bee0d
Mohamed Tarek <mohamed82008@gmail.com>
2021-11-30 21:19:37 +0200

    NN4TO example

--- test/examples/neural.jl
+++ test/examples/neural.jl
@@ -1,4 +1,5 @@
-using TopOpt
+using TopOpt, Makie, CairoMakie, Zygote
+Nonconvex.@load Ipopt
 
 E = 1.0 # Young’s modulus
 v = 0.3 # Poisson’s ratio
@@ -7,7 +8,7 @@ f = 1.0 # downward force
 problem = PointLoadCantilever(Val{:Linear}, (160, 40), (1.0, 1.0), E, v, f)
 
 V = 0.5
-xmin = 0.001
+xmin = 0.0001
 rmin = 4.0
 p = 3.0
 convcriteria = KKTCriteria()
@@ -21,8 +22,9 @@ filter = DensityFilter(solver; rmin)
 comp = Compliance(problem, solver)
 volfrac = Volume(problem, solver)
 
-m = 10
-nn = Chain(Dense(2, m, sigmoid), Dense(m, m, sigmoid), Dense(m, 1, sigmoid))
+m = 20
+act = relu
+nn = Chain(Dense(2, m, act), Dense(m, m, act), Dense(m, m, act), Dense(m, 1, sigmoid))
 nn_model = NeuralNetwork(nn, problem)
 tf = TrainFunction(nn_model)
 p0 = nn_model.init_params
@@ -31,29 +33,98 @@ tf(p0)
 obj = p -> comp(filter(tf(p)))
 constr = p -> volfrac(filter(tf(p))) - V
 
-alg = IpoptAlg()
-options = IpoptOptions()
+alg = MMA()
+options = MMAOptions()
 
 ##
+alg = IpoptAlg()
+options = IpoptOptions(max_iter = 200)
 model1 = Model()
 nparams = length(p0)
-addvar!(model1, fill(-10.0, nparams), fill(10.0, nparams))
-set_objective!(model1, constr)
+addvar!(model1, fill(-100.0, nparams), fill(100.0, nparams))
+set_objective!(model1, p -> (constr(p) + 0.1)^2)
 res1 = optimize(model1, alg, p0; options)
 ##
 
-model2 = Model()
-nparams = length(p0)
-addvar!(model2, fill(-10.0, nparams), fill(10.0, nparams))
-set_objective!(model2, obj)
-add_ineq_constraint!(model2, constr)
+struct LoggerFunction{F} <: Function
+    f::F
+end
+function (lf::LoggerFunction)(x)
+    @info "Solution extrema: $(extrema(x))"
+    val = lf.f(x)
+    @info "Objective value: $(val)"
+    return val
+end
+function ChainRulesCore.rrule(lf::LoggerFunction, x)
+    @info "Solution extrema: $(extrema(x))"
+    val, pb = Zygote.pullback(lf.f, x)
+    @info "Objective value: $(val)"
+    return val, Δ -> begin
+        temp = pb(Δ)
+        return (NoTangent(), temp[1])
+    end
+end
+
+# alg = IpoptAlg()
+# nparams = length(p0)
+# res2 = res1
+# for _ in 1:10
+#     global res2
+#     # Incremental optimization
+#     model2 = Model()
+#     addvar!(model2, res2.minimizer .- 1.0, res2.minimizer .+ 1.0)
+#     noise = randn(nparams)
+#     set_objective!(model2, LoggerFunction(p -> obj(p) + 0.1 * norm(p - res2.minimizer) + dot(noise, p)))
+#     # set_objective!(model2, LoggerFunction(obj))
+#     add_ineq_constraint!(model2, p -> constr(p))
+#     # res2 = optimize(model2, alg, res1.minimizer; options)
+#     options = IpoptOptions(max_iter = 10)
+#     res2 = optimize(model2, alg, res2.minimizer; options)
+
+#     # Feasibility restoration
+#     # model2 = Model()
+#     # addvar!(model2, res2.minimizer .- 2.0, res2.minimizer .+ 2.0)
+#     # set_objective!(model2, p -> norm(p - res2.minimizer))
+#     # add_ineq_constraint!(model2, p -> 100*constr(p))
+#     # options = IpoptOptions(max_iter = 5)
+#     # res2 = optimize(model2, alg, res2.minimizer; options)
+# end
+
+# model2 = Model()
+# addvar!(model2, fill(-10.0, nparams), fill(10.0, nparams))
+# noise = randn(nparams)
+# # set_objective!(model2, obj)
+# set_objective!(model2, p -> obj(p) + 0.1 * norm(p))
+# add_ineq_constraint!(model2, constr)
+# # add_ineq_constraint!(model2, p -> 10*constr(p))
+# options = IpoptOptions(max_iter = 300)
+# res2 = optimize(model2, alg, res1.minimizer; options)
+
+μ = 1.0
+res2 = res1
+for _ in 1:10
+    global μ *= 2
+    global res2
+    model2 = Model()
+    addvar!(model2, fill(-100.0, nparams), fill(100.0, nparams))
+    noise = randn(nparams)
+    # set_objective!(model2, obj)
+    set_objective!(model2, p -> μ * obj(p) - log(max(0, -constr(p))))
+    # add_ineq_constraint!(model2, p -> 10*constr(p))
+    options = IpoptOptions(max_iter = 100)
+    res2 = optimize(model2, alg, res2.minimizer; options)
+    @show extrema(tf(res2.minimizer))
+    @show obj(res2.minimizer)
+    @show constr(res2.minimizer)
+end
 
 Zygote.gradient(obj, p0)
 Zygote.gradient(constr, p0)
 
-res2 = optimize(model2, alg, res1.minimizer; options)
+tf(res2.minimizer)
 
 using Makie, CairoMakie
 
-topology = filter(res.minimizer)
+topology = filter(tf(res2.minimizer))
+using TopOpt.TopOptProblems.Visualization: visualize
 visualize(problem; topology)
