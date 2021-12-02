5ceb084f2d934180fd13fce75772eb3edab8e3a8
Mohamed Tarek <mohamed82008@gmail.com>
2021-12-02 04:37:03 +0200

    split CI

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,49 +1,52 @@
 using Test, SafeTestsets
 
-@safetestset "InpParser Tests" begin
-    include("inp_parser/parser.jl")
-end
-@safetestset "TopOptProblems Tests" begin
-    include("topopt_problems/problems.jl")
-    include("topopt_problems/metadata.jl")
-end
-@safetestset "Functions" begin
-    include("Functions/test_common_fns.jl")
-    include("Functions/test_buckling_fns.jl")
-end
-@safetestset "Solver" begin
-    include("fea/solvers.jl")
-end
-@safetestset "Truss Problem Tests" begin
-    include("truss_topopt_problems/test_problem.jl")
-    include("truss_topopt_problems/test_fea.jl")
-    include("truss_topopt_problems/test_buckling.jl")
-    include("truss_topopt_problems/test_buckling_optimize.jl")
+const GROUP = get(ENV, "GROUP", "All")
+
+if GROUP == "All" || GROUP == "Tests"
+    @safetestset "InpParser Tests" begin
+        include("inp_parser/parser.jl")
+    end
+    @safetestset "TopOptProblems Tests" begin
+        include("topopt_problems/problems.jl")
+        include("topopt_problems/metadata.jl")
+    end
+    @safetestset "Functions" begin
+        include("Functions/test_common_fns.jl")
+        include("Functions/test_buckling_fns.jl")
+    end
+    @safetestset "Solver" begin
+        include("fea/solvers.jl")
+    end
+    @safetestset "Truss Problem Tests" begin
+        include("truss_topopt_problems/test_problem.jl")
+        include("truss_topopt_problems/test_fea.jl")
+        include("truss_topopt_problems/test_buckling.jl")
+        include("truss_topopt_problems/test_buckling_optimize.jl")
+    end
 end
 
-@safetestset "Examples" begin
-    @safetestset "CSIMP" begin
+if GROUP == "All" || GROUP == "Examples"
+    @safetestset "CSIMP example" begin
         include("examples/csimp.jl")
     end
-    @safetestset "Global Stress" begin
+    @safetestset "Global stress example" begin
         include("examples/global_stress.jl")
     end
-    @safetestset "Local Stress" begin
+    @safetestset "Local stress example" begin
         include("examples/local_stress.jl")
     end
     @safetestset "More examples" begin
         include("examples/test_examples.jl")
     end
-    @safetestset "Neural networks" begin
+    @safetestset "Neural network example" begin
         include("examples/neural.jl")
     end
-    @safetestset "Integer nonlinear programming for truss optimization" begin
+    @safetestset "Integer nonlinear programming for truss optimization example" begin
         include("examples/mixed_integer_truss/truss_compliance_2d1.jl")
     end
 end
 
-
-@safetestset "WCSMO 14" begin
+if GROUP == "All" || GROUP == "WCSMO14"
     # This was originlly part of https://github.com/JuliaTopOpt/TopOpt.jl_WCSMO21
     @safetestset "Continuum demos" begin
         include("wcsmo14/demos/continuum/cont_compliance1.jl")
@@ -58,7 +61,7 @@ end
         include("wcsmo14/demos/truss/truss_compliance_3d1.jl")
         include("wcsmo14/demos/truss/truss_compliance_3d2.jl")
     end
-    @safetestset "Benchmarks" begin
+    @safetestset "WCSMO Benchmarks" begin
         include("wcsmo14/jl_benchmarks/compare_neo99_2D.jl")
         include("wcsmo14/jl_benchmarks/compare_polytop.jl")
         #include("wcsmo14/jl_benchmarks/compare_top3d.jl")
