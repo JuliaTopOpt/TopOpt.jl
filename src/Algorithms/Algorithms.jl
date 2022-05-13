15f07e77a7939dbbc9f6f61f094b33faf7e0c9fa
Mohamed Tarek <mohamed82008@gmail.com>
2022-05-13 02:15:51 +0200

    delete all gpu code

--- src/Algorithms/Algorithms.jl
+++ src/Algorithms/Algorithms.jl
@@ -2,7 +2,7 @@ module Algorithms
 
 using Nonconvex, ..Functions, Parameters, Requires
 using Nonconvex.NonconvexCore: AbstractModel
-using ..TopOpt: whichdevice, AbstractDevice, CPU, GPU, PENALTY_BEFORE_INTERPOLATION
+using ..TopOpt: whichdevice, AbstractDevice, CPU,PENALTY_BEFORE_INTERPOLATION
 using TimerOutputs, Setfield, StaticArrays
 using Parameters: @unpack, @pack!
 using ..Utilities, Ferrite
