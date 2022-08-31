c1500307c35174212d961641a7599f7c513efd29
Documenter.jl <documenter@juliadocs.github.io>
2022-08-31 14:18:17 +0000

    build based on 611a941

---
+++ previews/PR124/literate/local_stress.jl
@@ -0,0 +1,65 @@
+# using Revise
+using TopOpt, LinearAlgebra, StatsFuns
+using StatsFuns: logsumexp
+
+E = 1.0 # Young’s modulus
+v = 0.3 # Poisson’s ratio
+f = 1.0 # downward force
+rmin = 3.0
+
+# ### Define the problem
+problem = PointLoadCantilever(Val{:Linear}, (160, 40), (1.0, 1.0), E, v, f)
+
+# ### Parameter settings
+V = 0.5 # volume fraction
+xmin = 0.0001 # minimum density
+steps = 40 # maximum number of penalty steps, delta_p0 = 0.1
+
+# ### Continuation SIMP
+x0 = fill(0.5, 160 * 40) # initial design
+x = copy(x0)
+for p in [1.0, 2.0, 3.0]
+    global penalty, stress, filter, result, stress, x
+    penalty = TopOpt.PowerPenalty(p)
+    solver = FEASolver(Direct, problem; xmin=xmin, penalty=penalty)
+    stress = TopOpt.von_mises_stress_function(solver)
+    filter = DensityFilter(solver; rmin=rmin)
+    volfrac = TopOpt.Volume(solver)
+
+    obj = x -> volfrac(filter(PseudoDensities(x))) - V
+    thr = 150 # stress threshold
+    constr = x -> begin
+        s = stress(filter(PseudoDensities(x)))
+        return (s .- thr) / length(s)
+    end
+    alg = PercivalAlg()
+    options = PercivalOptions()
+    optimizer = Optimizer(obj, constr, x, alg; options=options)
+    simp = SIMP(optimizer, solver, p)
+    result = simp(x)
+    x = result.topology
+end
+
+maximum(stress(filter(PseudoDensities(x0))))
+maximum(stress(filter(PseudoDensities(x))))
+
+# ### (Optional) Visualize the result using Makie.jl
+# Need to run `using Pkg; Pkg.add(Makie)` first
+# ```julia
+# using Makie
+# using TopOpt.TopOptProblems.Visualization: visualize
+# fig = visualize(
+#    problem; topology = penalty.(filter(result.topology)), default_exagg_scale = 0.07,
+#    scale_range = 10.0, vector_linewidth = 3, vector_arrowsize = 0.5,
+# )
+# Makie.display(fig)
+# ```
+
+#md # ## [Plain Program](@id local-stress-plain-program)
+#md #
+#md # Below follows a version of the program without any comments.
+#md # The file is also available here: [local-stress.jl](local_stress.jl)
+#md #
+#md # ```julia
+#md # @__CODE__
+#md # ```
---
+++ previews/PR124/literate/simp.jl
@@ -0,0 +1,94 @@
+# # SIMP example: Point Load Cantilever 
+#-
+#md # !!! tip
+#md #     This example is also available as a Jupyter notebook:
+#md #     [`simp.ipynb`](@__NBVIEWER_ROOT_URL__/examples/simp.ipynb)
+#-
+# ## Commented Program
+#
+# What follows is a program spliced with comments.
+#md # The full program, without comments, can be found in the next [section](@ref simp-plain-program).
+
+using TopOpt
+
+# ### Define the problem
+E = 1.0 # Young’s modulus
+v = 0.3 # Poisson’s ratio
+f = 1.0; # downward force
+
+nels = (30, 10, 10)
+problem = PointLoadCantilever(Val{:Linear}, nels, (1.0, 1.0, 1.0), E, v, f);
+
+# See also the detailed API of `PointLoadCantilever`:
+#md # ```@docs
+#md # TopOpt.TopOptProblems.PointLoadCantilever
+#md # ```
+
+# ### Parameter settings
+V = 0.3 # volume fraction
+xmin = 1e-6 # minimum density
+rmin = 2.0; # density filter radius
+
+# ### Define a finite element solver
+penalty = TopOpt.PowerPenalty(3.0)
+solver = FEASolver(Direct, problem; xmin=xmin, penalty=penalty)
+
+# ### Define compliance objective
+comp = TopOpt.Compliance(solver)
+filter = DensityFilter(solver; rmin=rmin)
+obj = x -> comp(filter(PseudoDensities(x)))
+
+# ### Define volume constraint
+volfrac = TopOpt.Volume(solver)
+constr = x -> volfrac(filter(PseudoDensities(x))) - V
+
+# You can enable the iteration printouts with `Nonconvex.NonconvexCore.show_residuals[] = true`
+
+# ### Define subproblem optimizer
+mma_options =
+    options = MMAOptions(;
+        maxiter=3000, tol=Nonconvex.Tolerance(; x=1e-3, f=1e-3, kkt=0.001)
+    )
+convcriteria = Nonconvex.KKTCriteria()
+x0 = fill(V, length(solver.vars))
+optimizer = Optimizer(
+    obj, constr, x0, MMA87(); options=mma_options, convcriteria=convcriteria
+)
+
+# ### Define SIMP optimizer
+simp = SIMP(optimizer, solver, penalty.p);
+
+# ### Solve
+result = simp(x0);
+
+@show result.convstate
+@show optimizer.workspace.iter
+@show result.objval
+
+# ### (Optional) Visualize the result using Makie.jl
+# Need to run `using Pkg; Pkg.add(Makie)` first
+# ```julia
+# using TopOpt.TopOptProblems.Visualization: visualize
+# fig = visualize(problem; topology = result.topology,
+#     problem; topology = result.topology, default_exagg_scale = 0.07,
+#     scale_range = 10.0, vector_linewidth = 3, vector_arrowsize = 0.5,
+# )
+# Makie.display(fig)
+# ```
+
+# or convert it to a Mesh
+# Need to run `using Pkg; Pkg.add(GeometryBasics)` first
+# ```julia
+# import Makie, GeometryBasics
+# result_mesh = GeometryBasics.Mesh(problem, result.topology);
+# Makie.mesh(result_mesh)
+# ```
+
+#md # ## [Plain Program](@id simp-plain-program)
+#md #
+#md # Below follows a version of the program without any comments.
+#md # The file is also available here: [simp.jl](simp.jl)
+#md #
+#md # ```julia
+#md # @__CODE__
+#md # ```
