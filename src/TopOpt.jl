20903f3ead815260ffa7175b551fd25a7f048662
Mohamed Tarek <mohamed82008@gmail.com>
2023-09-25 05:25:15 +1000

    formatter toml and formatting

--- src/TopOpt.jl
+++ src/TopOpt.jl
@@ -18,12 +18,16 @@ function PseudoDensities{I,P,F}(x::A) where {I,P,F,T,N,A<:AbstractArray{T,N}}
     return PseudoDensities{I,P,F,T,N,A}(x)
 end
 
-Base.BroadcastStyle(::Type{T}) where {T <: PseudoDensities} =  Broadcast.ArrayStyle{T}()
-function Base.similar(bc::Broadcast.Broadcasted{Broadcast.ArrayStyle{T}}, ::Type{ElType}) where {T,ElType}
+Base.BroadcastStyle(::Type{T}) where {T<:PseudoDensities} = Broadcast.ArrayStyle{T}()
+function Base.similar(
+    bc::Broadcast.Broadcasted{Broadcast.ArrayStyle{T}}, ::Type{ElType}
+) where {T,ElType}
     return similar(T, axes(bc))
 end
-function Base.similar(::Type{<:TV}, axes::Tuple{Union{Integer, Base.OneTo}, Vararg{Union{Integer, Base.OneTo}}}) where {I, P, F, T, N, A, TV <: PseudoDensities{I,P,F,T,N,A}}
-    PseudoDensities{I,P,F}(similar(A, axes))
+function Base.similar(
+    ::Type{<:TV}, axes::Tuple{Union{Integer,Base.OneTo},Vararg{Union{Integer,Base.OneTo}}}
+) where {I,P,F,T,N,A,TV<:PseudoDensities{I,P,F,T,N,A}}
+    return PseudoDensities{I,P,F}(similar(A, axes))
 end
 
 function ChainRulesCore.rrule(
