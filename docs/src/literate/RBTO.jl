f4965a612741a8bc9b66dffe0df65290080ae99b
LucasMSpereira <lucas.lmspereira@gmail.com>
2024-03-29 10:17:03 -0300

    rbto script running

--- docs/src/literate/RBTO.jl
+++ docs/src/literate/RBTO.jl
@@ -1,5 +1,7 @@
-using ReliabilityOptimization, Test, NonconvexTOBS,
-    ChainRulesCore, TopOpt, Zygote, FiniteDifferences
+using ReliabilityOptimization, Test, ChainRulesCore
+using TopOpt, Zygote, FiniteDifferences, Nonconvex, NonconvexTOBS
+
+Nonconvex.@load MMA
 
 const v = 0.3 # Poisson’s ratio
 const densities = [0.0, 1.0] # for mass calc
@@ -16,15 +18,16 @@ filter = DensityFilter(solver; rmin = 3.0) # filter to avoid checkerboarding
 comp = Compliance(solver) # function that returns compliance
 penalty = TopOpt.PowerPenalty(3.0) # SIMP penalty
 const avgEs = [1e-6, 0.5]
-logEs = MvNormal(log.(avgEs), Matrix(Diagonal(0.1 .* avgEs .|> log .|> abs))) 
+logEs = MvNormal(log.(avgEs), Matrix(Diagonal(abs.(log.(avgEs) * 0.1))))
 # 'Original' function. At least one input is random.
 # In this example, Es is the random input.
-function compObj(y, Es)
+function compObj(y, logEs)
     penalty1 = TopOpt.PowerPenalty(3.0)
-    interp1 = MaterialInterpolation(Es, penalty1)
+    interp1 = MaterialInterpolation(exp.(logEs), penalty1)
     x = tounit(MultiMaterialVariables(y, nmats))
     _E = interp1(filter(x))
-    return comp(_E)
+    dsd = comp(_E)
+    return dsd
 end
 penalty2 = TopOpt.PowerPenalty(1.0)
 interp2 = MaterialInterpolation(densities, penalty2)
@@ -43,12 +46,13 @@ function obj(x) # objective for TO problem
     dist = rf(x)
     mean(dist)[1] + 2 * sqrt(cov(dist)[1, 1])
 end
-obj(x0)
-Zygote.gradient(obj, x0)
+# obj(x0)
+# Zygote.gradient(obj, x0)
 Zygote.gradient(constr, x0)
-FiniteDifferences.grad(central_fdm(5, 1), obj, x0)[1]
+# FiniteDifferences.grad(central_fdm(5, 1), obj, x0)[1]
 
 m = Model(obj) # create optimization model
 addvar!(m, zeros(length(x0)), ones(length(x0))) # setup optimization variables
 Nonconvex.add_ineq_constraint!(m, constr) # setup volume inequality constraint
-# @time r = Nonconvex.optimize(m, TOBSAlg(), x0; options = TOBSOptions())
+# @time r = Nonconvex.optimize(m, MMA87(), x0; options = MMAOptions())
+@time r = Nonconvex.optimize(m, TOBSAlg(), x0; options = TOBSOptions())
